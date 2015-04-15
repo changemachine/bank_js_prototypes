ecc010b809ded3bc3b0136fccaa9d28b1d43b497
erica cha & James Nielson <student@epicodus.com>
2015-04-15 16:34:35 -0700

    add picture, div for new column, math abs, commas to 1000. Extra special stuff. Success.

--- js/scripts.js
+++ js/scripts.js
@@ -20,15 +20,16 @@
     //global variable, will need it on both forms
     var myAccount;
 
-$(document).ready(function() {
 
+$(document).ready(function() {
+    $("#account-name").focus();
     //allows the document see whats going on beginning with the first submit form
     $("form#sign-up").submit(function(event) {
         event.preventDefault();
         // <----- this is the add name and amount box ----->
         //create new variables and save the input values from the prototype
         var inputName = $("input#account-name").val();
-        var inputBal = parseInt($("input#account-bal").val());
+        var inputBal =  Math.abs(parseInt($("input#account-bal").val()));
         //passing the prototype into the object to assign new values  to the properties
         myAccount = Object.create(BankAccount);
         myAccount.balance = inputBal;
@@ -36,7 +37,7 @@ $(document).ready(function() {
 
         //shows the name input 'myAccount.accountName', instead of 'this' due to being outside the object
         $("#account-name-display").text(myAccount.accountName);
-        $("#balance").text(myAccount.balance);
+        $("#balance").text(myAccount.balance.toLocaleString());
         //reveals the hidden deposit-withdraw and balance forms, hides the sign-up form
         $("#account-hide").show();
         //hides the new - account after entering the name and amount.
@@ -48,10 +49,11 @@ $(document).ready(function() {
         // <----- this is the add deposit or withdraw ----->
         $("#cha-ching").submit(function(event) {
             event.preventDefault();
+            // $("#deposit").focus();
             var inputDepositString = $("input#deposit").val();
             var inputWithdrawString = $("input#withdraw").val();
-            var inputDeposit = parseInt(inputDepositString);
-            var inputWithdraw = parseInt(inputWithdrawString);
+            var inputDeposit =  Math.abs(parseInt(inputDepositString));
+            var inputWithdraw =  Math.abs(parseInt(inputWithdrawString));
 
             if (inputDeposit >0 && inputWithdraw > 0) {
                 alert("Only 1 field at a time, please!")
@@ -60,8 +62,11 @@ $(document).ready(function() {
                 myAccount.deposit(inputDeposit);
                 myAccount.withdraw(inputWithdraw);
             }
-
-            $("#balance").text(myAccount.balance);
+            // var commaDisplay =
+            $("#balance").text(myAccount.balance.toLocaleString());
+            //this will empty the deposit/withdraw amount after entering the value
+            $("input#deposit").val('');
+            $("input#withdraw").val('');
 
         });
 
