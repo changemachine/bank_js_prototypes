97064f50718835133af37dd924d4316a39c6a97c
erica cha & James Nielson <student@epicodus.com>
2015-04-15 12:04:39 -0700

    input name and amount deposit works. the show for the accound-hide functions accordingly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,33 @@
-var BankAccount = {
-    balance: 0,
+$(document).ready(function() {
 
-    withdraw: function(amount) {
-        this.balance = this.balance - amount;
-    },
-    deposit: function(amount) {
-        this.balance = this.balance + amount;
-    }
-};
+    //requires the form under this 'document', allows the document see whats going on
+    $("form#sign-up").submit(function(event) {
+        event.preventDefault();
+    //prototype at the beginning, along with its properties and methods
+        var BankAccount = {
+            balance: 0,
+            accountName: '',
+            withdraw: function(amount) {
+                this.balance = this.balance - amount;
+            },
+            deposit: function(amount) {
+                this.balance = this.balance + amount;
+            }
+        };
+        //create new variables and save the input values in relation to the properties in the object
+        var inputName = $("input#account-name").val();
+        var inputBal = $("input#account-bal").val();
+        //passing the prototype into the object to assign new values  to the properties
+        var myAccount = Object.create(BankAccount);
+        myAccount.balance = inputBal;
+        myAccount.accountName = inputName;
+
+        //shows the name input
+        $('#account-name-display').text(this.accountName);
+        //hides the the deposit-withdraw and balance
+        $("#account-hide").show();
+
+
+    });
+
+});
